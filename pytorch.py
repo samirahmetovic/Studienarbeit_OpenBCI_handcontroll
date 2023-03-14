3a1cde5c1bdaefafa1a87a038cc46d4bbb07a27d
koenigmarcel <koenig.marcel@outlook.com>
2023-03-14 18:33:21 +0100

    3

--- pytorch.py
+++ pytorch.py
@@ -1,6 +1,7 @@
-import numpy as np
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import numpy as np
 
 # Load data from CSV file
 data = np.loadtxt('bci_data.csv', delimiter=',', skiprows=1)
@@ -9,53 +10,53 @@ data = np.loadtxt('bci_data.csv', delimiter=',', skiprows=1)
 inputs = torch.tensor(data[:, :-1], dtype=torch.float32)
 targets = torch.tensor(data[:, -1], dtype=torch.float32)
 
-
-class NeuralNet(nn.Module):
+# Define neural network architecture
+class EEGClassifier(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-        super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        super(EEGClassifier, self).__init__()
+        self.hidden = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
-        
+        self.output = nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()
+
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
+        x = self.hidden(x)
+        x = self.relu(x)
+        x = self.output(x)
+        x = self.sigmoid(x)
+        return x
 
 # Define hyperparameters
 input_size = inputs.shape[1]
-hidden_size = 10
-output_size = 2
-learning_rate = 0.001
+hidden_size = 8
+output_size = 1
+lr = 0.001
 num_epochs = 100
 
-# Initialize model and loss function
-model = NeuralNet(input_size, hidden_size, output_size)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# Initialize model, loss function, and optimizer
+model = EEGClassifier(input_size, hidden_size, output_size)
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=lr)
 
-# Train model
+# Train the model
 for epoch in range(num_epochs):
     # Forward pass
     outputs = model(inputs)
-    loss = criterion(outputs, targets.long())
+    loss = criterion(outputs, targets)
 
-    # Backward and optimize
+    # Backward pass and optimization
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-    # Print progress
-    if (epoch+1) % 10 == 0:
+    # Print loss for monitoring training progress
+    if epoch % 10 == 0:
         print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
-        
-        
-# Predict hand state for new EEG signals
-new_inputs = torch.tensor([[0.5, 0.4, 0.6, ..., 0.2]])
-outputs = model(new_inputs)
-_, predicted = torch.max(outputs.data, 1)
-if predicted.item() == 0:
-    print('Hand is closed')
+
+# Test the model on a new input
+test_input = torch.tensor([[0.5, 0.6, 0.4, 0.2, 0.1, 0.7]])
+prediction = model(test_input)
+if prediction >= 0.5:
+    print('Predicted class: closing hand')
 else:
-    print('Hand is open')
+    print('Predicted class: opening hand')
